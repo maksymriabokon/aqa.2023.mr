1bd011dcdee4da74d097d77bbaf3e2e9b761e814
Maksym Riabokon <maksym.riabokon@globallogic.com>
2023-10-24 17:11:20 +0300

    created new config file, and updated easy version

--- src/config/config_easy.py
+++ src/config/config_easy.py
@@ -1,16 +1,18 @@
+import json 
 import os
 
 class ConfigEasy:
   """Config class is responsible to storing framework's and env's configuration"""
   request_timeout = 40
-  user_name = os.environ.get ('USERNAME') # or get it from elsewhere
+  user_name = os.environ.get ('USERNAME') 
   env = os.environ.get ('AQA_ENV_MR')
-  home=os.environ.get ('HOME')
- 
+  my_os= os.environ.get('OS')
 
 config= ConfigEasy()
 
 print(config.request_timeout)
 print(config.user_name)
 print(config.env)
-print(config.home)
+print(config.my_os)
+
+
---
+++ tests/github/api/test_api.py
@@ -0,0 +1,13 @@
+from src.config.config_easy import ConfigEasy
+
+def test_api ():
+      print(ConfigEasy.env)
+ 
+    
+def test_api ():
+      print(ConfigEasy.request_timeout)
+
+def test_api ():
+      print(ConfigEasy.user_name)
+
+    
