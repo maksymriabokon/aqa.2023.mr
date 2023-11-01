bd7cb32386b2e38716c413891aa3087b84d54cdd
Maksym Riabokon <maksym.riabokon@globallogic.com>
2023-11-01 13:24:16 +0200

    modified files for api testing

--- tests/github/api/test_api.py
+++ tests/github/api/test_api.py
@@ -1,13 +1,34 @@
-from src.config.config_easy import ConfigEasy
+from src.applications.github.api.github_api import GitHubAPIClient
 
-def test_api ():
-      print(ConfigEasy.env)
- 
+
+def test_search_for_existing_repo():
+    github_api_client = GitHubAPIClient()
+
+    existing_repo_name = 'maksym'
+    repos = github_api_client.search_repos(existing_repo_name) # list/array returned
+
+    print("Checking total count is not 0")
     
-def test_api ():
-      print(ConfigEasy.request_timeout)
+    assert repos['total_count'] != 0  # some elements exists in array/list
 
-def test_api ():
-      print(ConfigEasy.user_name)
 
+def test_search_for_nonexisting_repo():
+    github_api_client = GitHubAPIClient()
     
+    nonexisting_repo_name = 'aaaaabbbbbbbbbbbbccccccccccccc'
+    repos = github_api_client.search_repos(nonexisting_repo_name) # list/array returned
+
+    print("Checking total count is 0")
+    
+    assert repos['total_count'] == 0  # some elements exists in array/list
+
+
+def test_search_for_nonexisting_repo():
+    github_api_client = GitHubAPIClient()
+    
+    nonexisting_commit_hasg = 'lnasdklfnkjanskdjfnkjabsdjhfuyagsuygfuyagsduyfgauysdf'
+    repos = github_api_client.search_commits(nonexisting_commit_hasg) # list/array returned
+
+    print("Checking total count is 0")
+    
+    assert 0 == 0  # some elements exists in array/list
