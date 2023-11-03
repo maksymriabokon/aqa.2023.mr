83df195668e7bef7f27c5eb07c693b0d4d1bfbd2
Maksym Riabokon <maksym.riabokon@globallogic.com>
2023-11-03 10:01:31 +0200

    updated api test

--- tests/github/api/test_api.py
+++ tests/github/api/test_api.py
@@ -5,30 +5,21 @@ def test_search_for_existing_repo():
     github_api_client = GitHubAPIClient()
 
     existing_repo_name = 'maksym'
-    repos = github_api_client.search_repos(existing_repo_name) # list/array returned
+    repos = github_api_client.search_repos(
+        existing_repo_name)  # list/array returned
 
     print("Checking total count is not 0")
-    
+
     assert repos['total_count'] != 0  # some elements exists in array/list
 
 
 def test_search_for_nonexisting_repo():
     github_api_client = GitHubAPIClient()
-    
+
     nonexisting_repo_name = 'aaaaabbbbbbbbbbbbccccccccccccc'
-    repos = github_api_client.search_repos(nonexisting_repo_name) # list/array returned
+    repos = github_api_client.search_repos(
+        nonexisting_repo_name)  # list/array returned
 
     print("Checking total count is 0")
-    
+
     assert repos['total_count'] == 0  # some elements exists in array/list
-
-
-def test_search_for_nonexisting_repo():
-    github_api_client = GitHubAPIClient()
-    
-    nonexisting_commit_hasg = 'lnasdklfnkjanskdjfnkjabsdjhfuyagsuygfuyagsduyfgauysdf'
-    repos = github_api_client.search_commits(nonexisting_commit_hasg) # list/array returned
-
-    print("Checking total count is 0")
-    
-    assert 0 == 0  # some elements exists in array/list
