c68c254f2452cbcede9b9b725153170429ea21ab
Maksym Riabokon <maksym.riabokon@globallogic.com>
2023-11-09 15:29:55 +0200

    add new ui test with object mod

--- src/applications/github/ui/github_ui.py
+++ src/applications/github/ui/github_ui.py
@@ -1,4 +1,30 @@
-class GitHubUI:
- """Current class contains every UI usage we use in tests"""
-def _init_(self) -> None:
-        pass
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class GitHubUILoginPage:
+
+    def __init__(self) -> None:
+        self.driver = webdriver.Edge()
+
+    def navigate_to_page(self):
+        self.driver.get("https://github.com/login")
+
+    def try_to_login(self, username, password):
+        username_field = self.driver.find_element(By.ID, 'login_field')
+        username_field.send_keys(username)
+
+        password_field = self.driver.find_element(By.ID, 'password')
+        password_field.send_keys(password)
+
+        login_button = self.driver.find_element(By.NAME, 'commit')
+        login_button.click()
+
+    def check_error_message(self):
+        error_message = self.driver.find_element(
+            By.CLASS_NAME, 'js-flash-alert')
+
+        return error_message is not None
+
+    def close_browser(self):
+        self.driver.quit()
--- tests/github/UI/test_ui.py
+++ tests/github/UI/test_ui.py
@@ -2,26 +2,44 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import pytest
+from src.applications.github.ui.github_ui import GitHubUILoginPage
 
 
 def test_github_login():
-    driver = webdriver.Edge()
+    github_login = webdriver.Edge()
 
-    driver.get("https://github.com/login")
+    github_login.get("https://github.com/login")
 
     time.sleep(3)
 
-    username_field = driver.find_element(By.ID, 'login_field')
+    username_field = github_login.find_element(By.ID, 'login_field')
     username_field.send_keys("incorrectLogin")
 
-    password_field = driver.find_element(By.ID, 'password')
+    password_field = github_login.find_element(By.ID, 'password')
     password_field.send_keys("Aaaaaaaaaaaa02")
 
     time.sleep(3)
 
-    login_button = driver.find_element(By.NAME, 'commit')
+    login_button = github_login.find_element(By.NAME, 'commit')
     login_button.click()
 
-    error_message = driver.find_element(By.CLASS_NAME, 'js-flash-alert')
+    error_message = github_login.find_element(By.CLASS_NAME, 'js-flash-alert')
     time.sleep(6)
     assert error_message is not None
+
+
+@pytest.fixture
+def github_login_page_object():
+    github_login = GitHubUILoginPage()
+    github_login.navigate_to_page()
+
+    yield github_login
+
+    github_login.close_browser()
+
+
+def test_github_login_negative_page_object(github_login_page_object):
+    github_login_page_object.try_to_login('weergbdgd', 'dfgegertgt45545')
+
+    assert github_login_page_object.check_error_message()
